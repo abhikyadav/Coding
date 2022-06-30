e4ef7a4ec137fb4e6c95822379064497e67b900a
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-06-30 11:07:51 +0530

    Time: 3 ms (97.85%), Space: 8.6 MB (73.61%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,65 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        // BFS Approach
+        if (!node)
+      return nullptr;
+
+    queue<Node*> q{{node}};
+    unordered_map<Node*, Node*> map{{node, new Node(node->val)}};
+
+    while (!q.empty()) {
+      Node* u = q.front();
+      q.pop();
+      for (Node* v : u->neighbors) {
+        if (!map.count(v)) {
+          map[v] = new Node(v->val);
+          q.push(v);
+        }
+        map[u]->neighbors.push_back(map[v]);
+      }
+    }
+
+    return map[node];
+    }
+        
+        //DFS APproach
+        
+//       if (!node)
+//       return nullptr;
+//     if (map.count(node))
+//       return map[node];
+
+//     Node* newNode = new Node(node->val);
+//     map[node] = newNode;
+
+//     for (Node* neighbor : node->neighbors)
+//       newNode->neighbors.push_back(cloneGraph(neighbor));
+
+//     return newNode;
+//   }
+
+//  private:
+//   unordered_map<Node*, Node*> map;
+};
