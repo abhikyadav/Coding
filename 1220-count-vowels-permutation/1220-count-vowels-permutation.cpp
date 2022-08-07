9f8374c4dcda1045b3216758e89860835f2f3d10
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-08-07 23:06:02 +0530

    Time: 7 ms (85.05%), Space: 6.5 MB (77.88%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,25 +1,36 @@
-static vector<vector<int>> relation={{1},{0,2},{0,1,3,4},{2,4},{0}};
 class Solution {
 public:
     int MOD=1e9+7;
-    vector<vector<int>> memo;
-    int dp(int n,int v)                         //'v' represents the vowel
+    vector<vector<int>> Multiply(vector<vector<int>> &l,vector<vector<int>> &r) //Multiply two matrices applying MOD.
     {
-        if(n==1)
-            return 1;
-        if(memo[n][v]!=-1)
-            return memo[n][v];
-        memo[n][v]=0;
-        for(int &i:relation[v])
-            memo[n][v]+=dp(n-1,i),memo[n][v]%=MOD;
-        return memo[n][v];
+        vector<vector<int>> result(l.size(),vector<int>(r[0].size(),0));
+        for(int i=0;i<l.size();i++)
+            for(int j=0;j<r[0].size();j++)
+                for(int k=0;k<l[0].size();k++)
+                    result[i][j]+=(long long)l[i][k]*r[k][j]%MOD,result[i][j]%=MOD;
+        return result;
     }
     int countVowelPermutation(int n) 
     {
-        int result=0;
-        memo.resize(n+1,vector<int>(5,-1));
-        for(int i=0;i<5;i++)
-            result+=dp(n,i),result%=MOD;
-        return result;
+        vector<vector<int>> M={ {0,1,0,0,0},      //Adjacency matrix of graph of the problem.
+                                {1,0,1,0,0},
+                                {1,1,0,1,1},
+                                {0,0,1,0,1},
+                                {1,0,0,0,0}},result(5,vector<int>(5));
+        for(int i=0;i<5;i++)   //Create identity Matrix.
+            result[i][i]=1;
+        int sum=0;
+        n--;
+        while(n)    //log(n) Multiplication.
+        {
+            if(n&1)
+                result=Multiply(M,result);
+            n>>=1;
+            M=Multiply(M,M);
+        }
+        for(vector<int> &i:result)          //Result holds M^(N-1).
+            for(int &j:i)
+                sum+=j,sum%=MOD;
+        return sum;
     }
 };
