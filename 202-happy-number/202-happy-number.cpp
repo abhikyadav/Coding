0809a9aa25348003dc74ef1166e11ba2b931bf90
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-08-11 01:56:24 +0530

    Time: 3 ms (63.20%), Space: 6.2 MB (43.46%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_map<int,int> mp;
+        while(n!=1)
+        {
+            if(mp[n]==0)
+            {
+                mp[n]++;
+            }
+            else
+            {
+                return false;
+            }
+            int sum =0;
+            while(n!=0)
+            {
+                sum+=pow(n%10,2);
+                n=n/10;
+            }
+            n=sum;
+        }
+        return true;
+    }
+};
