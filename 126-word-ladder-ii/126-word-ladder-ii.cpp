d4cea01df35410c4727d458a88c62899017ed7db
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-08-14 15:00:29 +0530

    Time: 27 ms (57.84%), Space: 9.5 MB (70.31%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.cpp
+++ 126-word-ladder-ii/126-word-ladder-ii.cpp
@@ -1,117 +1,90 @@
 class Solution {
 public:
     
-    bool neighbour(string a, string b){
-        int cnt = 0 ;
-        for(int i = 0 ; i < a.length() ; i++)
-        {
-            if(a[i] != b[i])
-            {
-                cnt++ ;
+    vector<int> neibs(string& a, unordered_map<string, int>& words) {
+        vector<int> ret;
+        for(char& x:a) {
+            char save = x;
+            for(char i='a';i<='z';i++) {
+                if(i!=save) {
+                    x = i;
+                    auto it = words.find(a);
+                    if(it!=words.end()) ret.push_back(it->second);
+                }
+            }
+            x = save;
+        }
+        return ret;
+    };
+    
+    struct step {
+        string & w;
+        vector<step*> prev;
+        int i = -1;
+        int depth = 0;
+        
+        step(string & word) : w(word) {};
+        step(string & word, int ci, step* p) : w(word), i(ci), prev({p}), depth(p->depth+1) {};
+    };
+    
+public:
+    void result(step* curr, vector<string>& path, vector<vector<string>>& ret) {
+        path.push_back(curr->w);
+        if(empty(curr->prev)) {
+            ret.push_back(vector<string>(path.rbegin(), path.rend()));
+        } else {
+            for(auto p:curr->prev) {
+                result(p, path, ret);
             }
         }
-        return cnt == 1 ;
-    }
+        path.pop_back();
+    };
+    
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        wordList.insert(wordList.begin(), beginWord);
-        for(int i = 1 ; i < wordList.size() ; i++)
-        {
-            if(wordList[i] == wordList[0]) // string compare
-            {
-                wordList[i] = wordList.back() ;
-                wordList.pop_back() ;
-                break ;
-            }
+        int n = wordList.size();
+        vector<bool> seen(n);
+        queue<step*> q;
+        vector<step*> reached(n);
+        
+        if(beginWord.back()=='"') beginWord.pop_back();
+        if(endWord.back()=='"') endWord.pop_back();
+
+        unordered_map<string, int> words;
+        for(int i=0;i<n;i++) {
+            words[wordList[i]] = i;
         }
-        map<string, int> wti ; // word to index
-        for(int i = 0 ; i < wordList.size() ; i++)
-        {
-            wti.insert({wordList[i], i}) ;
-        }
-        if(!wti.count(endWord)) 
-        {
-            return {} ;
-        }
-        vector<vector<int>> edges(wti.size()) ;
-        for(int i = 0 ; i < wordList.size() ; i++)
-        {
-            for(int j = 0 ; j < wordList.size() ; j++)
-            {
-                if(i != j)
-                {
-                    if(neighbour(wordList[i], wordList[j]))
-                    {
-                        edges[i].push_back(j) ;
+        if(words.count(beginWord)) seen[words[beginWord]] = true;
+        if(!words.count(endWord)) return {};
+        int endi = words[endWord];
+        
+        q.push(new step(beginWord));
+        while(!empty(q) && !reached[endi]) {
+            int s = q.size();
+            while(s--) {
+                step* curr = q.front();
+                q.pop();
+                if(curr->i>=0) seen[curr->i] = true;
+                
+                auto nbs = neibs(curr->w, words);
+                for(int i:nbs) {
+                    if(seen[i]) continue;
+                    if(reached[i]) {
+                        if(reached[i]->depth>curr->depth)
+                            reached[i]->prev.push_back(curr);
+                    } else {
+                        reached[i] = new step(wordList[i], i, curr);
+                        q.push(reached[i]);
                     }
                 }
             }
         }
-        // BFS  
-        int start_node = 0 , target_node = wti[endWord] , r = 0 , min_step = INT_MAX ;
-        vector<int> vis(wordList.size(), INT_MAX) ;   
-        vis[start_node] = 0 ;
-        queue<int> q ;   
-        q.push(start_node) ;
-        while(!q.empty())
-        {
-            int sz  = q.size() ;
-            for (int i = 0 ; i < sz ; i++)
-            {
-                int fr = q.front() ;  
-                q.pop() ;
-                if(fr == target_node)
-                {
-                    min_step = min(min_step , r) ;
-                }
-                for(int j = 0 ; j < edges[fr].size() ; j++)
-                {
-                    int update_node = edges[fr][j] ;
-                    if(r + 1 < vis[update_node])
-                    {
-                        vis[update_node] = r + 1 ;
-                        q.push(update_node);
-                    }
-                }
-            }
-            r++ ;
+
+        vector<vector<string>> ret;
+        if(reached[endi]) {
+            vector<string> path;
+            result(reached[endi], path, ret);
         }
-        if(min_step == INT_MAX)
-        {
-            return {} ;
-        }
-        queue<vector<string>> q2 ; 
-        q2.push({wordList[target_node]}) ;
-        r = min_step ;
-        while(r)
-        {
-             int sz  = q2.size() ;
-             for(int i = 0 ; i < sz ; i++)
-             {
-                vector<string> seq = q2.front() ;
-                q2.pop();
-                string back = seq.back() ;
-                int curr = wti[back] ;
-                for (int j = 0 ; j < edges[curr].size() ; j++)
-                {
-                    int new_node = edges[curr][j] ;
-                    if (vis[new_node] == r - 1)
-                    {
-                        seq.push_back(wordList[new_node]) ;
-                        q2.push(seq) ;
-                        seq.pop_back() ;
-                    }
-                }
-            }
-            r-- ;
-        }
-        vector<vector<string>> ans;
-        while(!q2.empty())
-        {
-            vector<string> temp = q2.front() ;
-            q2.pop() ;
-            reverse(begin(temp) , end(temp)) ;
-            ans.push_back(temp) ;
-        }
-        return ans ;
+        
+        return ret;
     }
 };
