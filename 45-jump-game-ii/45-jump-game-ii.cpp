51892c178f0658bae8f22458823e9989e8859715
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-08-19 20:57:04 +0530

    Time: 18 ms (86.96%), Space: 16.5 MB (96.72%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int jump(vector<int>& A) {
-       int jumps = 0, curEnd = 0, curFarthest = 0;
-	for (int i = 0; i < A.size() - 1; i++) {
-		curFarthest = max(curFarthest, i + A[i]);
-		if (i == curEnd) {
-			jumps++;
-			curEnd = curFarthest;
-		}
-	}
-	return jumps;
+    int jump(vector<int>& nums) {
+        if (nums.size() <= 1) return 0;
+        int curMax = 0; // to mark the last element in a level
+        int level = 0, i = 0;
+        while (i <= curMax) { 
+            int furthest = curMax; // to mark the last element in the next level
+            for (; i <= curMax; i++) {
+                furthest = max(furthest, nums[i] + i);
+                if (furthest >= nums.size() - 1) return level + 1;
+            }
+            level++;
+            curMax = furthest;
+        }
+        return -1;
     }
 };
