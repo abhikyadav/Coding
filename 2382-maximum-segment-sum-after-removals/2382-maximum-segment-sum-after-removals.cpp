b5b93120c771461372eab5dff1997f1ccd6b296b
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-08-20 21:39:45 +0530

    Time: 789 ms (16.67%), Space: 166 MB (16.67%) - LeetHub

--- 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
+++ 2382-maximum-segment-sum-after-removals/2382-maximum-segment-sum-after-removals.cpp
@@ -1,43 +1,72 @@
+struct Segment {
+    int start = -1;
+    int end = -1;
+    bool complete = false;
+    int64_t startSum = 0;
+    int64_t endSum = 0;
+    int64_t maxSum = 0;
+};
+
+ostream & operator << (ostream & os, Segment const& s) {
+	return os << s.maxSum;
+}
+
+
+void combine(Segment const& s1, Segment const& s2, Segment& ret) {
+    ret.end = s2.end != -1 ? s2.end : s1.end;
+    ret.start = s1.start != -1 ? s1.start : s2.start;
+    ret.complete = s1.complete && s2.complete;
+    
+    bool connect = s1.end == s2.start;
+    ret.startSum = s1.complete && connect ? s1.maxSum + s2.startSum : s1.startSum;
+    ret.endSum = s2.complete && connect ? s1.endSum + s2.maxSum : s2.endSum;
+    
+    ret.maxSum = max(s1.maxSum, s2.maxSum);
+    if (connect) {
+        ret.maxSum = max(ret.maxSum, s1.endSum + s2.startSum);
+    }
+}
+
 class Solution {
 public:
     vector<long long> maximumSegmentSum(vector<int>& nums, vector<int>& removeQueries) {
-                int n = nums.size();
-        multiset<long long> s;
-        s.insert(0);
-        set<pair<int, int>> in;
-        vector<long long> pre(n, nums[0]);
-        for(int i = 1; i < n; i++) pre[i] = pre[i - 1] + nums[i];
+        int n = nums.size();
+        vector<vector<Segment>> levels;
+        levels.push_back(vector<Segment>(n));
         
-        in.insert({0, n - 1});
-        
-        s.insert(pre[n - 1]);
-        
-        vector<long long> ans;
-        for(auto p : removeQueries) {
-            auto it = in.upper_bound({p, 2e9});
-            if(it == in.begin()) {
-                ans.push_back(0);
-                continue;
-            }
-            it--;
-            
-            long long L = (p ? pre[p - 1] : 0) - (it -> first ? pre[it -> first - 1] : 0);
-            long long R = pre[it -> second] - pre[p];
-            long long T = pre[it -> second] - (it -> first ? pre[it -> first - 1] : 0);
-            
-            s.erase(s.find(T));
-            if(it -> first <= p - 1) {
-                in.insert({it -> first, p - 1});
-                s.insert(L);
-            }
-            if(it -> second >= p + 1) {
-                in.insert({p + 1, it -> second});
-                s.insert(R);
-            }
-            
-            in.erase(it);
-            ans.push_back(*s.rbegin());
+        for (int i = 0; i < n; ++i) {
+            levels[0][i].start = i;
+            levels[0][i].end = i + 1;
+            levels[0][i].complete = true;
+            levels[0][i].startSum = nums[i];
+            levels[0][i].endSum = nums[i];
+            levels[0][i].maxSum = nums[i];
         }
-        return ans;
+        
+        while (levels.back().size() > 1) {
+            if (levels.back().size() % 2 == 1) {
+                levels.back().push_back(Segment());
+            }
+            levels.push_back(vector<Segment>());
+            vector<Segment>& next = levels.back();
+            vector<Segment>& prev = levels[levels.size() - 2];
+            
+            for (int i = 0; i < prev.size(); i += 2) {
+                Segment seg;
+                combine(prev[i], prev[i + 1], seg);
+                next.push_back(seg);
+            }
+        }
+        
+        vector<long long> ret; 
+        for (int q: removeQueries) {
+            levels[0][q] = Segment();
+            for (int level = 1; level < levels.size(); ++level) {
+                q /= 2;
+                combine(levels[level - 1][2 * q], levels[level - 1][2 * q + 1], levels[level][q]);
+            }
+            ret.push_back(levels.back().front().maxSum);
+        }
+        return ret;
     }
 };
