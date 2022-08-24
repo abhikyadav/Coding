fd083afde1fbd1bfaae435bfda2b82dbe7467716
abhikyadav <49395545+abhikyadav@users.noreply.github.com>
2022-08-24 10:35:00 +0530

    Time: 12 ms (81.40%), Space: 21.3 MB (74.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -22,7 +22,18 @@ public:
 public:
     
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root == NULL) return false;
-        return check(root, targetSum);
+        if( root == NULL )
+            return(false);
+
+        if(root -> left == NULL && root -> right == NULL)
+            return(root -> val == targetSum );
+        
+        if(hasPathSum(root -> left, targetSum - root -> val))
+            return(true);
+        
+        if(hasPathSum(root -> right, targetSum - root -> val))
+            return(true);
+        
+        return(false);
     }
 };
