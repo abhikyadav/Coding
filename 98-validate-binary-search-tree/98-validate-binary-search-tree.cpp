6b7f99c32888f5a9339db70ce1a180aec9b6d4e4
Abhishek Yadav <49395545+abhikyadav@users.noreply.github.com>
2022-09-05 05:30:57 +0530

    Time: 32 ms (7.83%), Space: 21.9 MB (15.64%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -11,15 +11,44 @@
  */
 class Solution {
 public:
-bool isValidBST(TreeNode* root) {
-        TreeNode* prev = NULL;
-        return validate(root, prev);
+// bool isValidBST(TreeNode* root) {
+//         TreeNode* prev = NULL;
+//         return validate(root, prev);
+//     }
+    // bool validate(TreeNode* node, TreeNode* &prev) {
+    //     if (node == NULL) return true;
+    //     if (!validate(node->left, prev)) return false;
+    //     if (prev != NULL && prev->val >= node->val) return false;
+    //     prev = node;
+    //     return validate(node->right, prev);
+    // }
+    vector<int> vec;
+    void inorder(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        inorder(root->left);
+        vec.push_back(root->val);
+        inorder(root->right);
     }
-    bool validate(TreeNode* node, TreeNode* &prev) {
-        if (node == NULL) return true;
-        if (!validate(node->left, prev)) return false;
-        if (prev != NULL && prev->val >= node->val) return false;
-        prev = node;
-        return validate(node->right, prev);
+    bool isValidBST(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return true;
+        }
+        inorder(root);
+        int n = vec.size();
+        for(int i=0; i<n-1; i++)
+        {
+            if(vec[i]>=vec[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+        
     }
 };
